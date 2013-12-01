3ac44f06bc3c92f0e2de6508921f71153aa04a9f
Savvin Sergei <sav1ch@hotmail.com>
2013-12-01 21:41:09 +0400

    Logic: added model factory

---
+++ Mathematical Epidemiology/Core/CompartmentModelFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MathematicalEpidemiology.Core
+{
+    static class CompartmentModelFactory
+    {
+        public static CompartmentModel CreateModel(CompartmentModelType type, bool isStochastic, State initialState,
+            Parameters parameters, double time, double timestep)
+        {
+            CompartmentModel model = null;
+            switch (type)
+            {
+                case CompartmentModelType.SIR:
+                    if (isStochastic)
+                        model = null;
+                    else
+                        model = new DeterministicSIR(initialState, parameters, time, timestep);
+                    break;
+            }
+            return model;
+        }
+    }
+}
