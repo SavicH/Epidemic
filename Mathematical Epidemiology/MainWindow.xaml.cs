f851e21c585b0bfbe3468c5915eaa66e9b844a05
Boldinova Natalya <boldinova_natalya@mail.ru>
2014-02-03 21:16:35 +0400

    Interface: Fixed SIS graphic

--- Mathematical Epidemiology/MainWindow.xaml.cs
+++ Mathematical Epidemiology/MainWindow.xaml.cs
@@ -125,7 +125,10 @@ namespace MathematicalEpidemiology
             {
                 infected[i] = solution[i, 2];
                 susceptible[i] = solution[i, 1];
-                recovered[i] = solution[i, 3];
+                if (model.CompartmentCount > 2)
+                {
+                    recovered[i] = solution[i, 3];
+                }
                 time[i] = solution[i, 0];
             }
             backgroundWorker.ReportProgress(100);
@@ -180,6 +183,7 @@ namespace MathematicalEpidemiology
             else if (rbSIS.IsChecked == true)
             {
                 modelType = CompartmentModelType.SIS;
+
             }
             else if (rbSEIR.IsChecked == true)
             {
