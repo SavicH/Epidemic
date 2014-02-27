25a040e8ee176a9723e1029b658deb4bc63d6a02
Boldinova Natalya <boldinova_natalya@mail.ru>
2014-02-27 14:35:21 +0400

    Logic: Imitation model: some changes

---
+++ Compartment models/Imitation/ImitationModel.cs
@@ -0,0 +1,83 @@
+﻿using Pigeon.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace CompartmentModels.Imitation
+{
+    public class ImitationModel : IModel
+    {
+       
+        private Timer timer = new Timer();
+        private IList<State> state = new List<State>();
+        private IList<ActorRef> agents = new List<ActorRef>();
+        private System.Threading.ManualResetEvent run = new System.Threading.ManualResetEvent(false);
+
+        private int timeInHours;
+        private bool isOver = false;
+
+        public ImitationModel(State initialState, Parameters parameters, double time)
+        {
+            this.timeInHours = (int)(time*Parameters.Hours+1);
+            SimpleAgent.AgentsState = new State();
+            var system = new ActorSystem(null);
+            ActorRef location = system.ActorOf(Props.Create(() => new SimpleLocation(agents)));
+
+            for (int i = 0; i < initialState.Susceptible; i++)
+            {
+                agents.Add(system.ActorOf(Props.Create(() => new SimpleAgent(location, Compartment.Susceptible, parameters))));
+            }
+            for (int i = 0; i < initialState.Infected; i++)
+            {
+                agents.Add(system.ActorOf(Props.Create(() => new SimpleAgent(location, Compartment.Infected, parameters))));
+            }
+            for (int i = 0; i < initialState.Removed; i++)
+            {
+                agents.Add(system.ActorOf(Props.Create(() => new SimpleAgent(location, Compartment.Removed, parameters))));
+            }
+
+            timer.Elapsed += timer_Elapsed;
+            timer.Interval = 20;
+            timer.Stop();
+        }
+
+        int count = 0;
+
+        void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            State tmp = SimpleAgent.AgentsState;
+            state.Add(tmp);
+            tmp.Time += 1.0/Parameters.Hours;
+            SimpleAgent.AgentsState = tmp;
+            foreach (var agent in agents)
+            {
+                agent.Tell(Messages.Time);
+            }
+            count++;  // tmp
+            if (count == timeInHours)
+            {
+                isOver = true;
+                run.Set();
+            }
+
+        }
+
+        public IList<State> Run()
+        {
+            timer.Start();
+            while (!isOver)    // very tmp
+            { }
+            run.Reset();  // doesn't work
+            timer.Stop();
+            foreach (var agent in agents)
+            {
+                agent.Stop();
+            }
+            return state;
+        }
+
+    }
+}
---
+++ Compartment models/Imitation/SimpleAgent.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Pigeon.Actor;
+
+namespace CompartmentModels.Imitation
+{
+    class SimpleAgent: UntypedActor
+    {
+        private const int rate = 1400; // tmp value
+        private static Parameters infectionParameters;
+        public static Parameters InfectionParameters
+        {
+            set
+            { 
+                infectionParameters = value;
+            }
+        }
+
+        private int currentTimeDisease;
+        public static Parameters parameters; // TMP
+       
+        private ActorRef currentLocation;
+        
+        public Compartment Compartment { get; private set; }
+
+        private static State agentsState = new State();
+        public static State AgentsState { get { return agentsState; } set {agentsState = value;} }
+
+        private static Random rand = new Random(DateTime.Now.Millisecond);
+
+        public SimpleAgent(ActorRef currentLocation, Compartment compartment, Parameters parameters)
+        {
+            SimpleAgent.parameters = parameters;
+            this.currentLocation = currentLocation;
+            switch (compartment)
+            {
+                case Compartment.Susceptible: Become(Susceptible); agentsState.Susceptible++; break;
+                case Compartment.Infected: Become(Infected); agentsState.Infected++; break;
+                case Compartment.Removed: Become(Removed); agentsState.Removed++;  break;
+            }
+        }
+
+        private bool IsInfected()
+        {
+           return rand.Next() % (parameters.InfectionRate*rate) == 0;
+        }
+
+        private void Susceptible(object message)
+        {
+            if (message == Messages.Infection)
+            {
+                if (IsInfected())
+                {
+                    Become(Infected);
+                    agentsState.Infected++;
+                    agentsState.Susceptible--;
+                }
+            }
+
+        }
+
+        private void Infected(object message)
+        {           
+            if (message == Messages.Time)
+            {
+                if (currentTimeDisease++ == parameters.DiseasePeriodInHours)
+                {
+                    Become(Removed);
+                    agentsState.Removed++;
+                    agentsState.Infected--;
+                }
+                currentLocation.Tell(Messages.Infection);
+            }          
+        }
+
+        private void Removed(object message)
+        {
+        
+        }
+
+
+        protected override void OnReceive(object message)
+        {
+
+        }
+    }
+}
